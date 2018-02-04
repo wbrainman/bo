d625b8c7525db65c3c16620a915427df4e966940
wangbo <wbrainman@163.com>
2018-02-04 20:40:15 +0800

    client add recv

--- socket/client1.c
+++ socket/client1.c
@@ -7,19 +7,21 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#define BUFFER_SIZE 2000
+
 int main()
 {
     int sockfd;
     int len;
     struct sockaddr_in address;
     int result;
-    char ch = 'A';
+    char buf[BUFFER_SIZE] = {0};
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = inet_addr("127.0.0.1");
-	address.sin_port = htons(9734);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr("192.168.0.106");
+    address.sin_port = htons(9734);
 
     len = sizeof(address);
 
@@ -29,9 +31,15 @@ int main()
         exit(1);
     }
 
-    write(sockfd, &ch, 1);
-    read(sockfd, &ch, 1);
-    printf("char from server = %c\n", ch);
+    while ( 1 ) {
+      if(0 < recv(sockfd, buf, BUFFER_SIZE, 0)) {
+        printf("recv data : %s\n", buf);
+        //memset((void*)buf, 0, BUFFER_SIZE);
+      }
+    }
+
+    printf("NO DATA\n");
+    
     close(sockfd);
     exit(0);
 }
