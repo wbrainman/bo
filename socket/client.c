240a43c73a611074d3aa2c799f880172cd92b51b
wangbo <wbrainman@163.com>
2018-02-25 19:33:22 +0800

    add makefile

--- socket/client.c
+++ socket/client.c
@@ -10,23 +10,39 @@
 #define THREAD_NUM 2
 
 
-int mian()
+int main()
 {
     int res;
     pthread_t t_recv;
     pthread_t t_send;
 
-    res = pthread_create(&t_recv, NULL, thread_recv); 
+	void *thread_result;
+
+    res = pthread_create(&t_recv, NULL, thread_recv, NULL); 
     if(res != 0) {
         perror("Thread recv creat failed"); 
         exit(EXIT_FAILURE);
     }
 
-    res = pthread_create(&t_recv, NULL, thread_send); 
+    res = pthread_create(&t_recv, NULL, thread_send, NULL); 
     if(res != 0) {
         perror("Thread send creat failed"); 
         exit(EXIT_FAILURE);
     }
+
+	res = pthread_join(t_recv, &thread_result);
+    if(res != 0) {
+        perror("Thread recv join failed"); 
+        exit(EXIT_FAILURE);
+    }
+
+	res = pthread_join(t_send, &thread_result);
+    if(res != 0) {
+        perror("Thread send join failed"); 
+        exit(EXIT_FAILURE);
+    }
+
+	printf("All done !!!\n");
 
 }
 
