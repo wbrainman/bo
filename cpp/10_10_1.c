67b275f8deb326b72856809a73a8cb89ba3eca51
wangbo <wbrainman@163.com>
2018-03-15 08:28:06 +0800

    add cpp file

---
+++ cpp/10_10_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <10_10_1.h>
+
+Count::count() 
+{
+	m_name = '\0'
+	m_countNum = '\0'
+	m_fund = 0;
+}
+
+Count::count() 
+{
+	m_name = '\0'
+	m_countNum = '\0'
+	m_fund = 0;
+}
---
+++ cpp/10_10_1.h
@@ -0,0 +1,17 @@
+#include <string.h>
+
+class Count {
+	private:
+		std::string	m_name;
+		std::string	m_countNum;
+		int 		m_fund;
+
+	public:
+		Count();
+		Count(const std::string &name, const std::string &countNum, const int fund);
+		~Count();
+		void show();
+		void deposite(const int in);
+		void withdraw(const int out);
+}
+
