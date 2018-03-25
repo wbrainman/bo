05c5ec8f0de0dea8949bb596f6296bbefd3f0d61
wangbo <wbrainman@163.com>
2018-03-26 01:03:16 +0800

    add cpp fime

--- cpp/10_10_1.cpp
+++ cpp/10_10_1.cpp
@@ -1,35 +1,46 @@
 #include <iostream>
 #include "10_10_1.h"
 
-//using namespace std;
+using namespace std;
 
 Count::Count() 
 {
-	m_name = '\0';
-	m_countNum = '\0';
+	m_name = "unknow";
+	m_countNum = "000000";
 	m_fund = 0;
 }
 
 Count::Count(const std::string &name, const std::string &countNum, const int fund)
-	:m_name('\0')
-	,m_countNum('\0')
-	,m_fund(0)
+	:m_name(name)
+	,m_countNum(countNum)
+	,m_fund(fund)
 {
 
 }
 
 void Count::show()
 {
+	cout << "name 		: "<< m_name << "\n";
+	cout << "countNum 	: "<< m_countNum << "\n";
+	cout << "fund 		: "<< m_fund << "\n";
 }
 
 void Count::deposite(const int in)
 {
-
+	cout << "deposit in : " << in << "\n";
+	m_fund += in;
+	cout << "fund now is : "<< m_fund << "\n";
 }
 
 void Count::withdraw(const int out)
 {
+	if(out > m_fund) {
+		cout << "sorry !, out > fund" << "\n";
+		return;
+	}
 
+	m_fund -= out;
+	cout << "fund now is : "<< m_fund << "\n";
 }
 
 Count::~Count()
