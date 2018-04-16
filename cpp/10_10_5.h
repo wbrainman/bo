88397b86a796ed026cfe66eaebd6899346ca4d50
wangbo <wbrainman@163.com>
2018-04-16 13:58:46 +0800

    add 10 10 5

---
+++ cpp/10_10_5.h
@@ -0,0 +1,23 @@
+//10_10_5.h
+
+#ifndef 10_10_5_H
+#define 10_10_5_H
+
+typedef struct customer {
+
+    char fullname[35];
+    double payment;
+}
+
+typedef customer Item;
+
+class Stack {
+
+
+
+
+}
+
+
+#endif
+
